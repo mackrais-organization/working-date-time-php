82885acb8d58816503e88c2c7290b14cfa67bd64
Oleh Boiko <minace002@gmail.com>
2025-03-15 00:41:54 +0200

    refactor and add tests

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PHP 8.4
+ * Created by PhpStorm.
+ *
+ * @author    : Oleh Boiko <support@mackrais.com> | <https://mackrais.com>
+ * @license   MIT License
+ * @copyright Copyright (c) 2016 - 2025, MackRais
+ */
+
+declare(strict_types=1);
+
+namespace MackRais\DateTime\Exception;
+
+interface ExceptionInterface extends \Throwable
+{
+}
